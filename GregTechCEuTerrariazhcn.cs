960431506fb7b8c3fccbd6149a778ccdccb9f843
TigerChenzzz <1139719265@qq.com>
2026-06-12 15:49:47 +0800

    任务书与硬编码汉化代码部分完成; 0.0.4 本地化文件更新

--- GregTechCEuTerrariazhcn.cs
+++ GregTechCEuTerrariazhcn.cs
@@ -1,6 +1,5 @@
-using MonoMod.Cil;
-using System.Reflection;
-using Terraria.ID;
+using GregTechCEuTerrariazhcnPublicizerPart;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using TigerForceLocalizationLib;
 using TigerForceLocalizationLib.Filters;
@@ -11,6 +10,7 @@ using TigerForceLocalizationLib.Filters;
 	正在等待的任务书: https://github.com/bereft-souls/QuestBooks
 	    据 Pringull 所说, Quest book 除了美术部分之外其余的都基本完成了 (Bereft Souls Discord at 2026/06/05 05:51)
 	作者 caxapexac 坦言任务书重做要花大量的时间 (Discord #development at 2026/06/09 07:13)
+    格雷科技汉化包下载: https://www.ningnana.top/index.php/15612-gregtech-community-packmodern-1-20-1
 可能需要特殊处理内容:
     任务书: GregTechCEuTerraria.TerrariaCompat.Questbook.QuestbookSystem.LoadQuestbook() 中的 "Data/Questbook/questlog.json"
     qb 任务书 (应该无需汉化): GregTechCEuTerraria.TerrariaCompat.QuestBooksInterop.GTQuestBookLoader.PostSetupContent() 中的 "Data/Questbook/qb-questlog.json"
@@ -25,6 +25,16 @@ namespace GregTechCEuTerrariazhcn
     public class GregTechCEuTerrariazhcn : Mod
     {
         private static bool Registering { get; } = false;
+
+        public override void Load()
+        {
+            LoadQuestbookHook();
+        }
+        public override void Unload()
+        {
+            UnloadQuestbookHook();
+        }
+
         public override void PostSetupContent()
         {
             if (Registering)
@@ -32,62 +42,38 @@ namespace GregTechCEuTerrariazhcn
                 TigerForceLocalizationHelper.ForceAllWeakReferences = false;
                 TigerForceLocalizationHelper.ShowLocalizationRegisterProgress();
             }
-            TigerForceLocalizationHelper.LocalizeFilters filters = !Registering ? null : new()
+            TigerForceLocalizationHelper.LocalizeFilters? filters = !Registering ? null : new()
             {
                 MethodFilter = MethodFilter.CommonMethodFilter,
                 CursorFilter = ILCursorFilter.CommonCursorFilter,
             };
+            TigerForceLocalizationHelper.LocalizeAll(nameof(GregTechCEuTerrariazhcn), "GregTechCEuTerraria", Registering, filters: filters);
         }
-        public void SomeHook()
+
+        #region 任务书汉化
+        private QuestbookLocalizer? QuestbookLocalizer { get; set; }
+
+        private string TranslateQuestbook(string key, string content)
         {
-            if (!ModLoader.TryGetMod("GregTechCEuTerraria", out var targetMod))
-            {
-                Logger.Error("Can't find Greg Mod!");
-                return;
-            }
-            var  targetAssembly = targetMod.GetType().Assembly;
-            var questbookSystemType = targetAssembly.GetType("GregTechCEuTerraria.TerrariaCompat.Questbook.QuestbookSystem");
-            if (questbookSystemType != null)
-            {
-                // TODO: 使用 hjson 扩展处理 hjson 文件
-                var setDataMethod = questbookSystemType.GetMethod("set_Data", BFALL);
-                if (setDataMethod == null)
-                {
-                    Logger.Warn("No set_Data method in type QuestbookSystem");
-                    return;
-                }
-                var loadQuestbookMethod = questbookSystemType.GetMethod("LoadQuestbook", BFALL);
-                if (loadQuestbookMethod == null)
-                {
-                    Logger.Warn("No LoadQuestbook method in type QuestbookSystem");
-                    return;
-                }
-                MonoModHooks.Modify(loadQuestbookMethod, il =>
-                {
-                    ILCursor cursor = new(il);
-                    if (!cursor.TryGotoNext(MoveType.AfterLabel, i => i.MatchCallOrCallvirt(setDataMethod)))
-                    {
-                        Logger.Warn("No set_Data call in method LoadQuestbook of type QuestbookSystem");
-                        return;
-                    }
-                    cursor.EmitDelegate((QuestLogData data) =>
-                    {
-                        // TODO: Localize the data
-                    });
-                    // 格雷科技汉化包下载: https://www.ningnana.top/index.php/15612-gregtech-community-packmodern-1-20-1
-                });
-            }
-            else
-            {
-                Logger.Warn("Can't find type QuestbookSystem");
-            }
+            var localizationKey = $"Mods.{nameof(GregTechCEuTerrariazhcn)}.Questbook.{key}";
+            return Language.GetOrRegister(localizationKey, () => content).Value;
         }
 
-        public const BindingFlags BFS = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
-        public const BindingFlags BFI = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-        public const BindingFlags BFALL = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+        private void LoadQuestbookHook()
+        {
+            // 由于目标模组注册任务书的时机是在 PostSetupContent, 所以 Hook 需要在此之前
+            QuestbookLocalizer?.Unload();
+            QuestbookLocalizer = new(TranslateQuestbook);
+            QuestbookLocalizer.Load();
+        }
+        private void UnloadQuestbookHook()
+        {
+            if (QuestbookLocalizer != null)
+            {
+                QuestbookLocalizer.Unload();
+                QuestbookLocalizer = null;
+            }
+        }
+        #endregion
     }
-
-    // TODO: 需要使用 Publicizer 得到 QuestLogData 类型
-    class QuestLogData;
 }
